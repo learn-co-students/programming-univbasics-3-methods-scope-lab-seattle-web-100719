56995a2e42281a88fd5467de6767a0088c3a1549
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-09-28 16:21:29 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,11 +9,10 @@ def toadstool
 end
 
 def link
-  warning = "It's Dangerous To Go Alone! Take This."
-  puts warning
+  puts "It's Dangerous To Go Alone! Take This."
 end
 
 def any_phrase (phrase)
-  phrase = "Do A Barrel Roll!"
+  # phrase = "Do A Barrel Roll!"
   puts phrase
 end
